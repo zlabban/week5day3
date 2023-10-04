82403686f6dc753eae45b23aa84e8e5c92882add
Ziad <ziadlabban@hotmail.co.uk>
2023-10-04 16:26:27 +0100

    connect.js

---
+++ connect.js
@@ -0,0 +1,19 @@
+// Import the pg package
+import pg from "pg";
+
+// Get the connection string using an environment variable
+const connectionString = process.env.DB_CONNECTION_STRING
+
+// Create a new pool instance
+const pool = new pg.Pool({
+    connectionString
+});
+
+// Send a query "SELECT * FROM books"
+const data = await pool.query("SELECT * FROM books;");
+const rows = data.rows;
+console.log(rows);
+
+// Close the connection
+await pool.end();
+
